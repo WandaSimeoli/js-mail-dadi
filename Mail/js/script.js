9f19c6edaab01aa96c8dffd68d11886265adac43
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-16 17:35:02 +0200

    Esercizio Mail

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,13 +1,47 @@
-const click = document.getElementById('click');
+// Lista di email
 
-click.addEventListener('click', 
-function() {
+const list = ['w@s.com', 'm@m.it', 't@z.com', 'i@m.com', 'l@s.it']; 
+console.log ('Lista di email', list, typeof list);
 
-const arr = [ email, @, effe, 
-];
-const value = effe;
- 
-const isFound = arr.includes(value);
-console.log(isFound);
-}
+// Sottomissione del form 
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit',
+    
+    function (event) {
+        
+        event.preventDefault();
+
+        const email = document.getElementById('email').value;
+        console.log(email, typeof email);
+
+        let check = false; 
+
+    // Ciclo for con la lista delle email da verificare 
+
+        for (let i = 0; i < list.length; i++) {
+            console.log (list[i]);
+        
+        if (email === list[i]) {
+            
+            check = true;
+
+        } 
+        // else {
+        //     check = false;
+        //     console.log(' non valido', check, typeof chek);
+        // }
+
+        if (check) {
+            alert('Email valida!');
+        } else {
+            alert('Email non valida!');
+        }
+        }
+    }
 );
+
+
+
+
